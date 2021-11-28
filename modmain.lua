5f343a7f13357330218bc64c6941833391345ce6
Jarek <95185558+JaroslawKargul@users.noreply.github.com>
2021-11-28 19:52:22 +0100

    Add files via upload

---
+++ modmain.lua
@@ -0,0 +1,416 @@
+PrefabFiles = {
+	"waroline",
+	"waroline_none",
+	
+	"sketchbook_waroline",
+	
+	"axe_drawing",
+	"pickaxe_drawing",
+	"campfire_drawing",
+	"campfirefire_drawing",
+	
+	"rabbit_drawing",
+	"spider_drawing",
+	"pigman_drawing",
+	"smallbird_drawing",
+	--"cookiecutter_drawing",
+	"beefalo_drawing",
+	"koalefant_drawing",
+	"monkey_drawing",
+	
+	"brokentool_drawing",
+}
+
+Assets = {
+	Asset("SOUNDPACKAGE", "sound/waroline.fev"),
+	Asset("SOUND", "sound/waroline.fsb"),
+
+    Asset( "IMAGE", "images/saveslot_portraits/waroline.tex" ),
+    Asset( "ATLAS", "images/saveslot_portraits/waroline.xml" ),
+
+    Asset( "IMAGE", "images/selectscreen_portraits/waroline.tex" ),
+    Asset( "ATLAS", "images/selectscreen_portraits/waroline.xml" ),
+	
+    Asset( "IMAGE", "images/selectscreen_portraits/waroline_silho.tex" ),
+    Asset( "ATLAS", "images/selectscreen_portraits/waroline_silho.xml" ),
+
+    Asset( "IMAGE", "bigportraits/waroline.tex" ),
+    Asset( "ATLAS", "bigportraits/waroline.xml" ),
+	
+	Asset( "IMAGE", "images/avatars/avatar_waroline.tex" ),
+    Asset( "ATLAS", "images/avatars/avatar_waroline.xml" ),
+	
+	Asset( "IMAGE", "images/avatars/avatar_ghost_waroline.tex" ),
+    Asset( "ATLAS", "images/avatars/avatar_ghost_waroline.xml" ),
+	
+	Asset( "IMAGE", "images/avatars/self_inspect_waroline.tex" ),
+    Asset( "ATLAS", "images/avatars/self_inspect_waroline.xml" ),
+	
+	Asset( "IMAGE", "images/names_waroline.tex" ),
+    Asset( "ATLAS", "images/names_waroline.xml" ),
+	
+	Asset( "IMAGE", "images/names_gold_waroline.tex" ),
+    Asset( "ATLAS", "images/names_gold_waroline.xml" ),
+	
+    Asset( "IMAGE", "bigportraits/waroline_none.tex" ),
+    Asset( "ATLAS", "bigportraits/waroline_none.xml" ),
+	
+	Asset( "IMAGE", "bigportraits/waroline_survivor.tex" ),
+    Asset( "ATLAS", "bigportraits/waroline_survivor.xml" ),
+	
+	Asset( "IMAGE", "bigportraits/waroline_winter.tex" ),
+    Asset( "ATLAS", "bigportraits/waroline_winter.xml" ),
+
+	Asset( "IMAGE", "images/hud/warolinesketchtab.tex" ),
+    Asset( "ATLAS", "images/hud/warolinesketchtab.xml" ),
+	
+	Asset( "IMAGE", "images/inventoryimages/sketchbook_waroline.tex" ),
+    Asset( "ATLAS", "images/inventoryimages/sketchbook_waroline.xml" ),
+	
+	Asset("ATLAS", "images/inventoryimages/axe_drawing.xml"),
+    Asset("IMAGE", "images/inventoryimages/axe_drawing.tex"),
+	
+	Asset("ATLAS", "images/inventoryimages/pickaxe_drawing.xml"),
+    Asset("IMAGE", "images/inventoryimages/pickaxe_drawing.tex"),
+	
+	Asset("ATLAS", "images/inventoryimages/campfire_drawing.xml"),
+    Asset("IMAGE", "images/inventoryimages/campfire_drawing.tex"),
+	
+	Asset("ATLAS", "images/inventoryimages/rabbit_drawing.xml"),
+    Asset("IMAGE", "images/inventoryimages/rabbit_drawing.tex"),
+	
+	Asset("ATLAS", "images/inventoryimages/spider_drawing.xml"),
+    Asset("IMAGE", "images/inventoryimages/spider_drawing.tex"),
+	
+	Asset("ATLAS", "images/inventoryimages/smallbird_drawing.xml"),
+    Asset("IMAGE", "images/inventoryimages/smallbird_drawing.tex"),
+	
+	Asset("ATLAS", "images/inventoryimages/pigman_drawing.xml"),
+    Asset("IMAGE", "images/inventoryimages/pigman_drawing.tex"),
+	
+	Asset("ATLAS", "images/inventoryimages/beefalo_drawing.xml"),
+    Asset("IMAGE", "images/inventoryimages/beefalo_drawing.tex"),
+	
+	Asset("ATLAS", "images/inventoryimages/koalefant_drawing.xml"),
+    Asset("IMAGE", "images/inventoryimages/koalefant_drawing.tex"),
+	
+	Asset("ATLAS", "images/inventoryimages/monkey_drawing.xml"),
+    Asset("IMAGE", "images/inventoryimages/monkey_drawing.tex"),
+	
+	-- Map icons
+	Asset( "IMAGE", "images/map_icons/waroline.tex" ),
+	Asset( "ATLAS", "images/map_icons/waroline.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_axe_drawing.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_axe_drawing.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_pickaxe_drawing.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_pickaxe_drawing.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_campfire_drawing.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_campfire_drawing.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_rabbit_drawing.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_rabbit_drawing.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_spider_drawing.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_spider_drawing.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_smallbird_drawing.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_smallbird_drawing.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_pigman_drawing.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_pigman_drawing.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_beefalo_drawing.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_beefalo_drawing.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_koalefant_drawing.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_koalefant_drawing.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_monkey_drawing.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_monkey_drawing.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_sketchbook_waroline.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_sketchbook_waroline.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_sketchbook_waroline_survivor.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_sketchbook_waroline_survivor.xml" ),
+	
+	Asset( "IMAGE", "images/map_icons/map_sketchbook_waroline_winter.tex" ),
+	Asset( "ATLAS", "images/map_icons/map_sketchbook_waroline_winter.xml" ),
+	
+	--
+	Asset("ANIM", "anim/broken_tool_drawing.zip"),
+	Asset("ANIM", "anim/waroline_idles.zip"),
+	Asset("ANIM", "anim/waroline_idles_mount.zip"),
+	Asset("ANIM", "anim/symbol_waroline_sketchbook.zip"),
+	Asset("ANIM", "anim/symbol_waroline_sketchbook_survivor.zip"),
+	Asset("ANIM", "anim/symbol_waroline_sketchbook_winter.zip"),
+	--
+	Asset("ANIM", "anim/waroline_idles2.zip"),
+}
+
+modimport("scripts/util/env.lua")
+
+AddMinimapAtlas("images/map_icons/map_axe_drawing.xml")
+AddMinimapAtlas("images/map_icons/map_pickaxe_drawing.xml")
+AddMinimapAtlas("images/map_icons/map_campfire_drawing.xml")
+AddMinimapAtlas("images/map_icons/map_rabbit_drawing.xml")
+AddMinimapAtlas("images/map_icons/map_spider_drawing.xml")
+AddMinimapAtlas("images/map_icons/map_smallbird_drawing.xml")
+AddMinimapAtlas("images/map_icons/map_pigman_drawing.xml")
+AddMinimapAtlas("images/map_icons/map_beefalo_drawing.xml")
+AddMinimapAtlas("images/map_icons/map_koalefant_drawing.xml")
+AddMinimapAtlas("images/map_icons/map_monkey_drawing.xml")
+AddMinimapAtlas("images/map_icons/map_sketchbook_waroline.xml")
+AddMinimapAtlas("images/map_icons/map_sketchbook_waroline_survivor.xml")
+AddMinimapAtlas("images/map_icons/map_sketchbook_waroline_winter.xml")
+
+RemapSoundEvent( "dontstarve/characters/waroline/death_voice", "waroline/waroline/death_voice" )
+RemapSoundEvent( "dontstarve/characters/waroline/hurt", "waroline/waroline/hurt" )
+RemapSoundEvent( "dontstarve/characters/waroline/talk_LP", "waroline/waroline/talk_LP" )
+RemapSoundEvent( "dontstarve/characters/waroline/yawn", "waroline/waroline/yawn" )
+RemapSoundEvent( "dontstarve/characters/waroline/emote", "waroline/waroline/emote" )
+RemapSoundEvent( "dontstarve/characters/waroline/ghost_LP", "waroline/waroline/ghost_LP" )
+RemapSoundEvent( "dontstarve/characters/waroline/pose", "waroline/waroline/pose" )
+RemapSoundEvent( "dontstarve/characters/waroline/eye_rub_vo", "waroline/waroline/eye_rub_vo" )
+RemapSoundEvent( "dontstarve/characters/waroline/carol", "waroline/waroline/carol" )
+
+-- Drawing sound effect
+RemapSoundEvent( "dontstarve/characters/waroline/drawing_sound", "waroline/waroline/waroline_drawing_sound" )
+
+AddMinimapAtlas("images/map_icons/waroline.xml")
+
+local require = GLOBAL.require
+local STRINGS = GLOBAL.STRINGS
+local RECIPETABS = GLOBAL.RECIPETABS
+local Recipe = GLOBAL.Recipe
+local Ingredient = GLOBAL.Ingredient
+local TECH = GLOBAL.TECH
+local TUNING = GLOBAL.TUNING
+local CHARACTER_INGREDIENT = GLOBAL.CHARACTER_INGREDIENT
+local CHARACTER_INGREDIENT_SEG = GLOBAL.CHARACTER_INGREDIENT_SEG
+local AllRecipes = GLOBAL.AllRecipes
+local SpawnPrefab = GLOBAL.SpawnPrefab
+local ACTIONS = GLOBAL.ACTIONS
+local RemovePhysicsColliders = GLOBAL.RemovePhysicsColliders
+local FRAMES = GLOBAL.FRAMES
+local ActionHandler = GLOBAL.ActionHandler
+local EventHandler = GLOBAL.EventHandler
+local State = GLOBAL.State
+local TimeEvent = GLOBAL.TimeEvent
+local GetValidRecipe = GLOBAL.GetValidRecipe
+local FOODTYPE = GLOBAL.FOODTYPE
+local GetGameModeProperty = GLOBAL.GetGameModeProperty
+
+local PREFAB_SKINS = GLOBAL.PREFAB_SKINS
+local PREFAB_SKINS_IDS = GLOBAL.PREFAB_SKINS_IDS
+local SKIN_AFFINITY_INFO = GLOBAL.require("skin_affinity_info")
+
+-- Tuning values
+TUNING.WAROLINE_HEALTH = GetModConfigData("warolinehp") or 150
+TUNING.WAROLINE_SANITY = GetModConfigData("warolinesanity") or 200
+TUNING.WAROLINE_HUNGER = GetModConfigData("warolinehunger") or 150
+
+-- Max amount of sketches Waroline can have at once. Cannot go below 1 and above 3.
+TUNING.WAROLINE_SKETCH_LIMIT = GetModConfigData("warolinesketchlimit") or 1
+
+TUNING.STARTING_ITEM_IMAGE_OVERRIDE.sketchbook_waroline = {atlas = "images/inventoryimages/sketchbook_waroline.xml", image = "sketchbook_waroline.tex" }
+TUNING.GAMEMODE_STARTING_ITEMS.DEFAULT.WAROLINE = {"sketchbook_waroline"}
+
+TUNING.AXE_DRAWING_USES = 30 -- Enough to chop down 2 fully grown trees, but not more
+TUNING.AXE_DRAWING_DAMAGE = 17 -- Lowest canon melee damage - same as Walking Cane
+
+TUNING.PICKAXE_DRAWING_USES = 12 -- Enough to mine 2 rocks, but not more
+TUNING.PICKAXE_DRAWING_DAMAGE = 17 -- Lowest canon melee damage - same as Walking Cane
+
+TUNING.DRAWING_ATTACK_DAMAGE = 100 -- Drawings deal a lot of damage but die in one hit
+TUNING.DRAWING_MOUNT_DAMAGE = 34 -- Yes, mounts are weaker, because they are player-controlled...
+
+local seg_time = 30
+TUNING.WAROLINEDRAWING_SANITYAURA_TINY = 100/(seg_time*32)
+TUNING.WAROLINEDRAWING_SANITYAURA_SMALL_TINY = 100/(seg_time*20)
+TUNING.WAROLINEDRAWING_SANITYAURA_SMALL = 100/(seg_time*8)
+
+TUNING.SKETCHBOOK_WAROLINE = {}
+TUNING.SKETCHBOOK_WAROLINE.DAMAGE = 17
+TUNING.SKETCHBOOK_WAROLINE.PROTOTYPERS = {
+	["rabbit_drawing"] = { "rabbit" },
+	["spider_drawing"] = { "spider", "spider_warrior" },
+	["smallbird_drawing"] = { "smallbird", "teenbird", "tallbird" },
+	["pigman_drawing"] = { "pigman", "pigguard", "moonpig" },
+	["beefalo_drawing"] = { "beefalo" },
+	["koalefant_drawing"] = { "koalefant_summer", "koalefant_winter" },
+	["monkey_drawing"] = { "monkey" },
+}
+TUNING.SKETCHBOOK_WAROLINE.SREPYTOTORP = {
+	["rabbit"] = "rabbit_drawing",
+	["spider"] = "spider_drawing",
+	["spider_warrior"] = "spider_drawing",
+	["smallbird"] = "smallbird_drawing",
+	["teenbird"] = "smallbird_drawing",
+	["tallbird"] = "smallbird_drawing",
+	["pigman"] = "pigman_drawing",
+	["pigguard"] = "pigman_drawing",
+	["moonpig"] = "pigman_drawing",
+	["beefalo"] = "beefalo_drawing",
+	["koalefant_summer"] = "koalefant_drawing",
+	["koalefant_winter"] = "koalefant_drawing",
+	["monkey"] = "monkey_drawing",
+}
+
+local axe_drawing_cost = GetModConfigData("drawingaxecost") or 15
+local pickaxe_drawing_cost = GetModConfigData("drawingpickaxecost") or 15
+local campfire_drawing_cost = GetModConfigData("drawingcampfirecost") or 25
+local rabbit_drawing_cost = GetModConfigData("drawingrabbitcost") or 30
+local spider_drawing_cost = GetModConfigData("drawingspidercost") or 30
+local smallbird_drawing_cost = GetModConfigData("drawingsmallbirdcost") or 40
+local pigman_drawing_cost = GetModConfigData("drawingpigmancost") or 50
+local beefalo_drawing_cost = GetModConfigData("drawingbeefalocost") or 80
+local koalefant_drawing_cost = GetModConfigData("drawingkoalefantcost") or 80
+local monkey_drawing_cost = GetModConfigData("drawingmonkeycost") or 70
+
+TUNING.SKETCHBOOK_WAROLINE.PREFABS = {
+	["axe_drawing"] = { ingredientvalue = axe_drawing_cost, tech = "NONE", sortkey = 1 },
+	["pickaxe_drawing"] = { ingredientvalue = pickaxe_drawing_cost, tech = "NONE", sortkey = 2 },
+	["campfire_drawing"] = { ingredientvalue = campfire_drawing_cost, tech = "NONE", sortkey = 3 },
+	["rabbit_drawing"] = { ingredientvalue = rabbit_drawing_cost, tech = "RABBIT_DRAWING_ONE", sortkey = 4 },
+	["spider_drawing"] = { ingredientvalue = spider_drawing_cost, tech = "SPIDER_DRAWING_ONE", sortkey = 5 },
+	["smallbird_drawing"] = { ingredientvalue = smallbird_drawing_cost, tech = "SMALLBIRD_DRAWING_ONE", sortkey = 6 },
+	["pigman_drawing"] = { ingredientvalue = pigman_drawing_cost, tech = "PIGMAN_DRAWING_ONE", sortkey = 7 },
+	["beefalo_drawing"] = { ingredientvalue = beefalo_drawing_cost, tech = "BEEFALO_DRAWING_ONE", sortkey = 8 },
+	["koalefant_drawing"] = { ingredientvalue = koalefant_drawing_cost, tech = "KOALEFANT_DRAWING_ONE", sortkey = 9 },
+	["monkey_drawing"] = { ingredientvalue = monkey_drawing_cost, tech = "MONKEY_DRAWING_ONE", sortkey = 10 },
+}
+
+-- List of crops Sketch Rabbit can hold it its mouth - if a crop is not here, the crop will be just picked and left on the ground
+TUNING.RABBIT_DRAWING_SUPPORTED_VEGGIES = {
+	"asparagus",
+	"carrot",
+	"corn",
+	"dragonfruit",
+	"durian",
+	"eggplant",
+	"garlic",
+	"onion",
+	"pepper",
+	"pomegranate",
+	"potato",
+	"pumpkin",
+	"tomato",
+}
+
+TUNING.RABBIT_DRAWING_SUPPORTED_CROPS = {
+	"farm_plant_asparagus",
+	"farm_plant_carrot",
+	"farm_plant_corn",
+	"farm_plant_dragonfruit",
+	"farm_plant_durian",
+	"farm_plant_eggplant",
+	"farm_plant_garlic",
+	"farm_plant_onion",
+	"farm_plant_pepper",
+	"farm_plant_pomegranate",
+	"farm_plant_potato",
+	"farm_plant_pumpkin",
+	"farm_plant_tomato",
+}
+
+-- This exists for OnLoad sketch spawn handling. Because other ways had a chance of failing.
+TUNING.GLOBAL_SKETCH_ONLOAD_DEPOSITORY = {}
+
+TUNING.WAROLINE_DRAWING_DISTANCE = 8
+
+-- Crafting Tab
+WarolineSketchTab = AddRecipeTab("Sketchbook", 248182, "images/hud/warolinesketchtab.xml", "warolinesketchtab.tex", "waroline")
+
+modimport("scripts/util/drawing_prototyper.lua")
+for prefab,postinit_data in pairs(TUNING.SKETCHBOOK_WAROLINE.PROTOTYPERS) do 
+	if prefab ~= nil then
+		CreateNewDrawingPrototyper(prefab, postinit_data)
+	end
+end
+
+local function AllowDrawingInProximity(pt)
+	-- This is actually run on the server, not on client, so we cannot check ThePlayer
+	local ents = GLOBAL.TheSim:FindEntities(pt.x, pt.y, pt.z, TUNING.WAROLINE_DRAWING_DISTANCE, {"sketchbook_user"})
+    return #ents > 0
+end
+
+local sketchbook_waroline_recipe = AddRecipe("sketchbook_waroline", 
+{Ingredient("papyrus", 1), Ingredient("feather_crow", 1)},
+RECIPETABS.MAGIC, 
+TECH.NONE, 
+nil, 
+nil, 
+nil, 
+nil, 
+"waroline", 
+"images/inventoryimages/sketchbook_waroline.xml", 
+"sketchbook_waroline.tex")
+sketchbook_waroline_recipe.sortkey = -21345732987491385793285783295984375983475734983789573495734986769
+sketchbook_waroline_recipe.skinnable = true
+
+PREFAB_SKINS["sketchbook_waroline"] = { "sketchbook_waroline_survivor", "sketchbook_waroline_winter" }
+PREFAB_SKINS["sketchbook_waroline_survivor"] = { "sketchbook_waroline_winter" }
+PREFAB_SKINS["sketchbook_waroline_winter"] = { "sketchbook_waroline" }
+
+-- Automate recipe creation
+for prefab,craftingdata in pairs(TUNING.SKETCHBOOK_WAROLINE.PREFABS) do
+	if prefab ~= nil then
+		local new_recipe = AddRecipe(prefab, 
+		{Ingredient(CHARACTER_INGREDIENT.HUNGER, craftingdata.ingredientvalue)},
+		WarolineSketchTab, 
+		TECH[craftingdata.tech], 
+		(prefab .. "_placer"), 
+		1.3, 
+		nil, 
+		nil, 
+		"sketchbook_user", 
+		("images/inventoryimages/" .. prefab .. ".xml"), 
+		(prefab .. ".tex"))
+		new_recipe.isdrawing = true
+		new_recipe.build_distance = TUNING.WAROLINE_DRAWING_DISTANCE
+		new_recipe.testfn = AllowDrawingInProximity
+		new_recipe.sortkey = craftingdata.sortkey
+		new_recipe.no_deconstruction = true
+	end
+end
+
+modimport("scripts/strings.lua")
+
+-- CHARACTER SKINS
+
+modimport("scripts/util/waroline_skin_api.lua")
+modimport("scripts/util/waroline_skin_items_api.lua")
+modimport("scripts/postinits/skins_longsleeves_fix.lua")
+
+SKIN_AFFINITY_INFO.waroline = { "waroline_survivor", "waroline_winter" }
+PREFAB_SKINS["waroline"] = { "waroline_none", "waroline_survivor", "waroline_winter" }
+
+if PREFAB_SKINS_IDS == nil then
+	PREFAB_SKINS_IDS = {}
+end
+for prefab,skins in pairs(PREFAB_SKINS) do
+    PREFAB_SKINS_IDS[prefab] = {}
+    for k,v in pairs(skins) do
+      	PREFAB_SKINS_IDS[prefab][v] = k
+    end
+end
+
+AddModCharacter("waroline", "FEMALE")
+AddSkinnableCharacter("waroline") 
+
+--- [[[ -- POSTINITS -- ]]] ---
+
+modimport("scripts/postinits/misc.lua")
+
+--- [[[ -- ACTIONS -- ]]] ---
+
+modimport("scripts/postinits/actions_player.lua")
+
+--- [[[ -- STATEGRAPHS -- ]]] ---
+
+modimport("scripts/postinits/stategraph_creatures.lua")
+modimport("scripts/postinits/stategraph_player.lua")
